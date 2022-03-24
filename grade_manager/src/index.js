4befbf1a4c4483c1952facd4cf837d12049be146
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-03-24 11:36:53 -0400

    updating course component, dropdown hover functionality not working

---
+++ grade_manager/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Style/index.css";
+import App from "./js/App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- grade_manager/src/js/App.js
+++ grade_manager/src/js/App.js
@@ -1,21 +1,18 @@
-import './App.css';
+import "../Style/App.css";
+import Course from "./Course";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">        
+      <header className="App-header">
         <div className="helloWorld">
-          <p>Hello World</p>
+          <Course></Course>
+
           {/* add names here to make sure that you can push to github and make a pull from helloWorld to main */}
-          <p>Varsha Vattappally</p>
-          <p>Anjeza Beca</p>
-          <p>Keshav Saraogi</p>
-          <p>Jerrell Lewis</p>
         </div>
-       
       </header>
     </div>
   );
-}  
+}
 
 export default App;

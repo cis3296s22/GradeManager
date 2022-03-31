ec05d9edd6450064daf1fba54dbf8f4cd9247dae
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-03-31 14:06:07 -0400

    profile base

--- grade_manager/src/js/App.js
+++ grade_manager/src/js/App.js
@@ -1,12 +1,14 @@
 import "../Style/App.css";
 import Course from "./Course";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div className="App">
       {/*add router when adding new pages (login/signup)  */}
       <h1>Grade Manager</h1>
-      {<Course></Course>}
+      {/* {<Course></Course>} */}
+      <Profile></Profile>
     </div>
   );
 }

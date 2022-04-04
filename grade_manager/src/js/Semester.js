553ba825d52b8b6531b00efebad7fb43a6228e5e
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-04-04 17:01:39 -0400

    removed comments

--- grade_manager/src/js/Semester.js
+++ grade_manager/src/js/Semester.js
@@ -4,31 +4,13 @@ import { useState } from "react";
 import { ListGroupItem } from "react-bootstrap";
 
 function Semester() {
-  // const[newSemester, setNewSemester] = useState({
-  //   semester
-  // })
-  function addSemesterTab(e) {
-    console.log("should add new semester here?");
-    //  put all the semster into an array? and map through
-    // add new semester to this array and render it below instead
-    //  render a new semester? and send it to the database?
-
-    // SEND TO DATABASE
-  }
-
   // pull the semesters from the database
   // semesterobject : {
   // course1: {assignment1: {name: "name", grade: 100, group: "quiz"}};
 
-  function semesterCall(semester) {
+  function addSemesterTab(semester) {
     // automatically increment the semester &
     // SEND TO DATABASE
-
-    // semesterNames.concat(
-    //   "Semester " +
-    //     1 +
-    //     Number(semesterNames[semesterNames.length - 2].match(/\d+/g))
-    // );
 
     setSemesterNames((prevState) => [
       ...prevState,
@@ -50,17 +32,7 @@ function Semester() {
     "Semester 7",
     "Semester 8",
   ]);
-  // const semesterNames = [
-  //   "Semester 1",
-  //   "Semester 2",
-  //   "Semester 3",
-  //   "Semester 4",
-  //   "Semester 5",
-  //   "Semester 6",
-  //   "Semester 7",
-  //   "Semester 8",
-  //   "Add New Semester",
-  // ];
+
   const listItems = semesterNames.map((semester) => (
     <ListGroup.Item
       action
@@ -78,7 +50,7 @@ function Semester() {
       <ListGroupItem
         action
         eventKey={"Add New Semester"}
-        onClick={semesterCall}
+        onClick={addSemesterTab}
       >
         Add New Semester
       </ListGroupItem>

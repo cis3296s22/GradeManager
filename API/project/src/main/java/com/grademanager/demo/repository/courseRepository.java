0f13d802a3e85b890f82fa6d845e1d229c21acca
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-04-10 11:39:37 -0400

    courseRepo

--- API/project/src/main/java/com/grademanager/demo/repository/courseRepository.java
+++ API/project/src/main/java/com/grademanager/demo/repository/courseRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface courseRepository extends JpaRepository<Student,Long> {    
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
 }

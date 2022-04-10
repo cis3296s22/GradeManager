8d60602a174a661f44d3ddf55a24d96dad6ba568
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-04-10 18:38:11 -0400

    updating Course
    create/update CourseController, CourseService, CourseRepository files

--- API/project/src/main/java/com/grademanager/demo/controller/CourseController.java
+++ API/project/src/main/java/com/grademanager/demo/controller/CourseController.java
@@ -1,18 +1,53 @@
 package com.grademanager.demo.controller;
-import com.grademanager.demo.model.*;
+import java.util.Optional;
+
+import com.grademanager.demo.model.Course;
+import com.grademanager.demo.service.courseService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/Student")
+@RequestMapping("api/v1/course")
 public class CourseController {
 
-    @PostMapping("/scores")
-    public Integer quizScore(@RequestBody Id studentId){
-        return 0;
+    @Autowired
+    courseService courseService;
+
+    //GET
+    @GetMapping("{course}")
+    public Course getCourse(Integer id){
+        Optional <Course> optionalCourse = courseService.getCourse(id);
+        if(!optionalCourse.isPresent()){
+            String err = String.format("The course having ID %s was not found", id);
+            System.out.println(err);
+        }
+        return optionalCourse.get();
+    }
+
+    // POST
+    @PostMapping
+    public void createNewCourse(@RequestBody Course course){
+        courseService.createCourse(course);
+    }
+
+    @PostMapping("/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable Integer id){
+        courseService.updateCourse(course);
+    }
+
+    // DELETE
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable Integer id){
+        courseService.deleteCourse(id);
+
     }
 }
--- API/project/src/main/java/com/grademanager/demo/service/courseService.java
+++ API/project/src/main/java/com/grademanager/demo/service/courseService.java
@@ -1,30 +1,41 @@
 package com.grademanager.demo.service;
 
+import java.util.Optional;
 import com.grademanager.demo.model.Course;
 import com.grademanager.demo.repository.CourseRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import lombok.extern.log4j.Log4j2;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@Service
+@Log4j2
 public class courseService {
 
-    
     @Autowired
     CourseRepository courseRepository;
 
-
-    public Boolean deleteCourse(Long studentId){
-        Boolean success = false;
-    
-        try{
-            if(courseRepository.findById(studentId).isPresent()){
-                courseRepository.deleteById(studentId);
-                success = true;
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    return success;
+    public void createCourse(Course course){
+        courseRepository.save(course);
     }
 
-    
+    public Optional<Course> getCourse(Integer id){
+        return courseRepository.findById(id);
+    }
+
+    public List<Course> getAllCourses(){
+        List<Course> courses = new ArrayList<>();
+        courseRepository.findAll().forEach(courses::add);
+        return courses;
+    }
+
+    public void deleteCourse(Integer id){
+        courseRepository.deleteById(id);
+    }
+
+    public void updateCourse(Course course){
+        courseRepository.save(course);
+    }
 }

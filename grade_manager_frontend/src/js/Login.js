3887ab6b4a22361ada0c01a3a4d9592194c57b31
Anjeza <tun54437@temple.edu>
2022-04-10 18:32:34 -0400

    Login same as SignUp page

--- grade_manager_frontend/src/js/Login.js
+++ grade_manager_frontend/src/js/Login.js
@@ -1,45 +1,100 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "../Style/LoginPageLayout.css";
+import '../Style/LoginPageLayout.css';
+import React from "react";
 
-export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const initialState = {
+    email: "",
+    password: ""
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
+};
+
+
+export default class Login extends React.Component {
+
+    state = initialState
+
+    handleChange = event => {
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+            [event.target.name]: isCheckbox
+                ? event.target.checked
+                : event.target.value
+        });
+    };
+
+    validate = () =>{
+
+        let emailError = "";
+        let passwordError = "";
+
+
+        if (!this.state.email){
+            emailError = "Enter your Email!";
+        }
+        if (!this.state.email.includes("@")){
+            emailError = "Invalid Email";
+        }
+
+        if (!this.state.password){
+            passwordError = "Please Enter the Password";
+        }
+
+        return true;
     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
+    handleButtonClicked = event => {
+        const isValid = this.validate();
+        if (isValid){
+            alert('Welcome');
+            this.setState(initialState);
+        }
+    };
 
-    return (
-        <div className="Login">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group size="lg" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <Button block size="lg" type="submit" disabled={!validateForm()}>
-                    Login
-                </Button>
-            </Form>
-        </div>
-    );
+
+    render() {
+        return (
+            <div className = "LoginPageLayout">
+                <div className = "LoginFormat">
+                    <h2 className = "LoginHeading">Login!</h2>
+                    <form className = "LoginForm">
+
+
+
+                        <input
+                            className = "NameInputText"
+                            type = "text"
+                            name = "email"
+                            value = {this.state.email}
+                            onChange = {this.handleChange}
+                            placeholder = "Email"
+                        />
+                        <div style = {{ fontSize: 12, color: "red"}}>
+                            {this.state.emailError}
+                        </div>
+
+                        <input
+                            className = "NameInputText"
+                            type = "password"
+                            name = "Password"
+                            value = {this.state.password}
+                            onChange = {this.handleChange}
+                            placeholder = "Password"
+                        />
+                        <div style = {{ fontSize: 12, color: "red"}}>
+                            {this.state.passwordError}
+                        </div>
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleButtonClicked}>
+
+                            Login</button>
+
+                    </form>
+
+                </div>
+            </div>
+        );
+    }
 }
-

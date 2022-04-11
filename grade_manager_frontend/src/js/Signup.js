da2cbbbe51c006cc27aa14508ffe084438972395
Jerrell <tuk57480@temple.edu>
2022-04-11 03:04:37 -0400

    Made changes to the sign up file. All validations are working and input fields.

--- grade_manager_frontend/src/js/Signup.js
+++ grade_manager_frontend/src/js/Signup.js
@@ -5,8 +5,8 @@ const initialState = {
   name: "",
   date: "",
   email: "",
-  student: "",
-  teacher: "",
+  student: "checkbox",
+  teacher: "checkbox",
   password: "",
   password_2: "",
   nameError: "",
@@ -15,72 +15,88 @@ const initialState = {
   studentError: "",
   teacherError: "",
   passwordError: "",
-  password_2Error: ""
+  password_2Error: "",
 };
 
 
 export default class Signup extends React.Component {
      
-    state = initialState
+  state = initialState
 
-    handleChange = event => {
-      const isCheckbox = event.target.type === "checkbox";
-      this.setState({
-        [event.target.name]: isCheckbox
-          ? event.target.checked
-          : event.target.value
-      });
-    };
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
 
-    validate = () =>{
+  validate = () =>{
+    
+    this.state.nameError = "";
+    this.state.dateError = "";
+    this.state.emailError = "";
+    this.state.passwordError = "";
+    this.state.password_2Error = "";
+
+    if (!this.state.name){
+      this.state.nameError = "Please Enter A Name!";
+    }
+
+    if (!this.state.date){
+        this.state.dateError = "Select A Date of Birth!";
+      }
+
+    if (!this.state.email){
+        this.state.emailError = "Enter An Email!";
+      }
+      else if (!this.state.email.includes("@") || !this.state.email.includes(".")){
+        this.state.emailError = "Invalid Email, missing '@' or '.' ";
+      }     
       
-      let nameError = "";
-      let dateError = "";
-      let emailError = "";
-      let passwordError = "";
-      let password_2Error = "";
-      
-      
-      if (!this.state.name){
-        nameError = "Please Enter A Name!";
+    if (!this.state.student.checked && !this.state.teacher.checked){
+        this.state.studentError = "Select One Of The Above";
+    }
+
+    if (!this.state.password){
+        this.state.passwordError = "Please Enter A Password";
       }
 
-      if (!this.state.date){
-        dateError = "Select A Date of Birth!";
-      }
-
-      if (!this.state.email){
-        emailError = "Enter An Email!";
-      }
-
-      if (!this.state.email.includes("@")){
-        emailError = "Invalid Email";
-      }      
-
-      if (!this.state.password){
-        passwordError = "Please Enter A Password";
-      }
-
-      if (!this.state.password_2){ 
-        password_2Error = "Please Enter A Password";
+    if (!this.state.password_2){ 
+        this.state.password_2Error = "Please Enter A Password";
       }
 
       if (this.state.password != this.state.password_2){
-        passwordError = "Passwords Do Not Match!";
-        password_2Error = "Passwords Do Not Match!";      
+        this.state.passwordError = "Passwords Do Not Match!";
+        this.state.password_2Error = "Passwords Do Not Match!";      
       }
 
-      if (this.state.password.length() < 8){
-        passwordError = "Password must be at least 8 characters long!";
+      if (this.state.password.length < 8){
+        this.state.passwordError = "Password must be at least 8 characters long!";
       }
 
       if (this.state.password_2.length() < 8){ 
-        password_2Error = "Password must be at least 8 characters long!";
+        this.state.password_2Error = "Password must be at least 8 characters long!";
       }
 
-      if (nameError || dateError || emailError || passwordError || password_2Error){
-        this.state({nameError, dateError, emailError, passwordError, password_2Error});
-        return true;
+      if (this.state.nameError || this.state.dateError || this.state.emailError || this.state.studentError || this.state.passwordError /*|| this.state.password_2Error*/){
+        alert("Please Check Written Messages In Red. Please type random character in any given input to display the various text.");
+        if (this.state.name){
+          this.setState(this.state.nameError);
+        }
+        if (this.state.date){
+          this.setState(this.state.dateError);
+        }
+        if (this.state.email){
+          this.setState(this.state.emailError);
+        }
+        if (this.state.student || this.state.teacher){
+          this.setState(this.state.studentError);
+        }
+
+        this.setState(initialState);
+        return false;
       }
 
       return true;
@@ -89,7 +105,17 @@ export default class Signup extends React.Component {
     handleButtonClicked = event => {
       const isValid = this.validate();
       if (isValid){
-        alert('Welcome');
+        alert('Welcome ' + this.state.name);
+        this.state.name = "";
+        this.state.date = "";
+        this.state.email = "";
+        this.state.password = "";
+        this.state.password_2 = "";
+        this.state.nameError = "";
+        this.state.dateError = "";
+        this.state.emailError = "";
+        this.state.passwordError = "";
+        this.state.password_2Error = "";
         this.setState(initialState);
       }
     };
@@ -110,21 +136,25 @@ export default class Signup extends React.Component {
           onChange = {this.handleChange}
           placeholder = "Name"
           />
+          <label className="ErrorMessages">
           <div style = {{ fontSize: 12, color: "red"}}>
             {this.state.nameError}
           </div>
+          </label>
 
           Date of Birth<input
           className = "NameInputText"
           type = "date"
-          name = "Date Of Birth"
+          name = "date"
           value = {this.state.date}
           onChange = {this.handleChange}
-          placeholder = "D.O.B"
           />
+
+          <label className="ErrorMessages">
           <div style = {{ fontSize: 12, color: "red"}}>
             {this.state.dateError}
           </div>
+          </label>
 
         <input
           className = "NameInputText"
@@ -134,9 +164,11 @@ export default class Signup extends React.Component {
           onChange = {this.handleChange}
           placeholder = "Email"
           />
+          <label className="ErrorMessages">
           <div style = {{ fontSize: 12, color: "red"}}>
             {this.state.emailError}
           </div>
+          </label>
 
           <label className= "InstructText">
           <p>Select Whether You Are A Student Or A Teacher</p>
@@ -147,7 +179,7 @@ export default class Signup extends React.Component {
           <input
           className = "InputText"
           type = "radio"
-          name = "Teacher"
+          name = "radio"
           value = {this.state.teacher}
           onChange = {this.handleChange}
           /> 
@@ -157,35 +189,45 @@ export default class Signup extends React.Component {
           <input
           className = "InputText"
           type = "radio"
-          name = "Student"
+          name = "radio"
           value = {this.state.student}
           onChange = {this.handleChange}
           label = "Student"
           /> 
+          <label className="ErrorMessages">
+          <div style = {{ fontSize: 12, color: "red"}}>
+            {this.state.studentError}
+          </div>
+          </label>
 
           <input
           className = "NameInputText"
           type = "password"
-          name = "Password"
+          name = "password"
           value = {this.state.password}
           onChange = {this.handleChange}
           placeholder = "Password"
           />
+          <label className="ErrorMessages">
           <div style = {{ fontSize: 12, color: "red"}}>
             {this.state.passwordError}
           </div>
+          </label>
 
           <input
           className = "NameInputText"
           type = "password"
-          name = "Re-enter Password"
-          value = {this.state.password_2}
+          name = "re-password"
+          value = {this.state.password2}
           onChange = {this.handleChange}
-          placeholder = "Re-enter Password"
+          placeholder = "Enter Password Again"
           />
+          <label className="ErrorMessages">
           <div style = {{ fontSize: 12, color: "red"}}>
-            {this.state.password_2Error}
+            {this.state.password2Error}
           </div>
+          </label>
+
           <p>Passwords Must Be At Least 8 Characters Long</p>
 
           <button 

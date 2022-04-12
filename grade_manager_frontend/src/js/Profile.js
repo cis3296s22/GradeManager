5d9e5acdb93b13c93b4bb76d092a4f0b9c64b934
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-04-12 02:36:36 -0400

    profile

--- grade_manager_frontend/src/js/Profile.js
+++ grade_manager_frontend/src/js/Profile.js
@@ -2,46 +2,60 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import { useEffect, useState } from "react";
-import { FaArchway } from "react-icons/fa";
+import Container from "react-bootstrap/Container";
+
+import image from "../icon/profileImage.png";
 
 function Profile() {
   const styles = {
     profileDistance: {
       //   position: "absolute",
+      //   width: "75%",
+      //   justifyContent: "center",
       //   top: "5rem",
       //   left: "25rem",
-      // figure out how to center this godamn thing
+
+      backgroundColor: "grey",
     },
   };
   const [profileInfo, setProfileInfo] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch("STUDENT PROFILE URL HERE "); //replace URL here with student
-      const json = await data.json();
-      console.log(json);
-      setProfileInfo(json);
+      //   STUDENT PROFILE URL BELOW
+      const data = await fetch("https://dog.ceo/api/breeds/image/random"); //replace URL here with student
+      if (data.ok) {
+        const json = await data.json();
+        console.log(json);
+        setProfileInfo(json);
+        setProfileInfo({
+          name: "remove this setProfileInfo",
+          email: "test@gmail.com",
+          age: 90,
+          studentId: 89899,
+        });
+      }
     };
     fetchData();
   }, []);
+  console.log("will continue to get errors without the URL above");
   console.log(profileInfo);
   // {"studentId":8403576665033886403,"email":"hi","birthday":null,"age":1,"semesterList":[],"name":"022078925508001002461285"}
   return (
-    <Row style={styles.profileDistance}>
-      <Col>
-        {/* "src/icon/profileImage.png" */}
-        <Image
-          src={"https://covers.openlibrary.org/b/isbn/0062060619-M.jpg"}
-          rounded
-        ></Image>
-      </Col>
-      <Col>
-        <Row>{profileInfo.name}</Row>
-        <Row>{profileInfo.email}</Row>
-        <Row>{profileInfo.age && <span> Years Old</span>}</Row>
-        <Row>{profileInfo.studentId}</Row>
-      </Col>
-    </Row>
+    <Container>
+      <Row style={styles.profileDistance}>
+        <Col>
+          {/* "src/icon/profileImage.png" */}
+          <Image src={image} rounded></Image>
+        </Col>
+        <Col>
+          <Row>{profileInfo.name}</Row>
+          <Row>{profileInfo.email}</Row>
+          <Row>{profileInfo.age} Years Old</Row>
+          <Row>{profileInfo.studentId}</Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

1aeea1833208a5a9a7ed8071910c3f749884fece
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-04-17 11:02:52 -0400

    quiz endpoints working but some are missing responses

--- API/project/src/main/java/com/grademanager/demo/repository/QuizRepository.java
+++ API/project/src/main/java/com/grademanager/demo/repository/QuizRepository.java
@@ -1,9 +1,9 @@
 package com.grademanager.demo.repository;
-import java.util.Optional;
+// import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.grademanager.demo.model.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    Optional<Quiz> deleteByName(String name);
+    
 
 }

2e6549565ec781ec5d27729f3743a73f046f4b8b
JerrellLewis <89744097+JerrellLewis@users.noreply.github.com>
2022-04-18 03:06:21 -0400

    Update App.js

--- grade_manager_frontend/src/js/App.js
+++ grade_manager_frontend/src/js/App.js
@@ -1,33 +1,20 @@
-import {
-  Link, Route, Routes
-} from 'react-router-dom';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import "../Style/App.css";
 import Course from "./Course";
 import Signup from "./Signup";
 import Login from "./Login";
-import { BrowserRouter } from 'react-router-dom';
+
 
 export default function App(){
 
   return (
     <BrowserRouter>
-        {<Login></Login>}
-        <div className = "App">
-          <nav>
-          <ul>
-            <li>
-            <Link to = "/Signup" target = {"_blank"}>Signup</Link>
-            </li>
-          </ul>
-          </nav>
-          <switch>
-          <Routes>
+        <Routes>
+          <Route path ="/" element = {<Login></Login>}/>  
           <Route path="/Signup" element = {<Signup></Signup>}/>
+          <Route path="/Home" element = {<Course></Course>}/>
           </Routes>
-          </switch>
-          </div>
-    
+          
     </BrowserRouter>
   );
 }
-

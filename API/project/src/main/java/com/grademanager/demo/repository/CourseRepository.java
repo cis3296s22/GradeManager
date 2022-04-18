e96e852b31160af9cf29f4e20b7b08922508c317
Varsha V <63315944+vvattap@users.noreply.github.com>
2022-04-18 14:25:34 -0400

    new Course Repository file

---
+++ API/project/src/main/java/com/grademanager/demo/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package com.grademanager.demo.repository;
+
+import com.grademanager.demo.model.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    
+
+}
